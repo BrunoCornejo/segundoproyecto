d9678c81ab003c00f93581fe5a152586781bcccc
Facundo Diaz <dfacundo905@gmail.com>
2025-10-26 14:24:10 -0300

    se creo la seccion main y su carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import FormLogin from './components/auth/FormLogin.jsx';
+import Portada from './home/Portada.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage /> 
+    <div >
+      <Portada/> 
     </div>
   );
 }
