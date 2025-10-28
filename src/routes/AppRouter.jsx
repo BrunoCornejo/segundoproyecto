ee051270632da2f638bc1ade4c61622f5dd99ae2
Facundo Diaz <dfacundo905@gmail.com>
2025-10-28 20:44:00 -0300

    corregimos de nuevo las ramas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,11 +5,14 @@ import LoginPage from "../pages/LoginPage.jsx";
 import Adminpage from "../pages/Adminpage.jsx";
 import Error404Page from "../pages/Error404Page.jsx";
 import RegisterPage from "../pages/RegisterPage.jsx";
+import Header from "../components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 
 export default function AppRouter() {
     return (
         <>
+         <Header/>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/registro" element={<RegisterPage />} />
@@ -21,6 +24,7 @@ export default function AppRouter() {
             <Route path="*" element={<Error404Page />} />
             
         </Routes>
+        <Footer/>
         </>
     );
 }
