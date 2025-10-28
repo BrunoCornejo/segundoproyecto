f6fa3f917d05590589648c8424b8b277ec1bfdfb
Facundo Diaz <dfacundo905@gmail.com>
2025-10-28 19:19:48 -0300

    se creo la seccion de categorias

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import FormLogin from './components/auth/FormLogin.jsx';
 import Portada from './home/Portada.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import Error404 from './pages/Error404Page.jsx';
+import Header from "./components/Header.jsx";
+import Categorias from "./home/Categorias.jsx";
 
 function App() {
   return (
-    <div >
+    <>
+      <Header/>
       <Portada/> 
-    </div>
+      <Categorias/>
+    </>
   );
 }
 
